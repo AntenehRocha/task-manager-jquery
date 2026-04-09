88a9f49acff79d9894a325fe3ac74f95df322cc8
Anteneh Rocha <antenehrochaaotero@gmail.com>
2026-04-09 11:46:23 +0200

    añadidos de la clase de refuerzo, cambio de generar tarea enumerada a tener placeholder para añadir titulo y descripcion

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 $(document).ready(function () {
-    let counter = 1;
+  // let counter = 1;
 
+  /** 
     $("#create-task").click(function () {
-        $("#container").prepend(`
+      $("#container").prepend(`
+        <div class="each-task">
+          <input type="checkbox">
+          <p>Tarea ${counter}</p>
+        </div>
+      `);
+      */
+
+  // let taskTitle = $("#task-input").val("");
+  // let taskDescription = $("#description-input").val("");
+
+  let event = $("#create-task").click(function () {
+    let taskTitle = $("#task-input").val();
+    let taskDescription = $("#description-input").val();
+    console.log(taskTitle);
+    console.log(taskDescription);
+
+    $("#container").prepend(` 
       <div class="each-task">
-        <input type="checkbox">
-        <p>Tarea ${counter}</p>
+        <p> Title: ${taskTitle} </p>
+        <p> Description: ${taskDescription} </p>
+        <button id="tick-completed">Completed</button>
+        <button id="tick-not-completed">No Completed</button>
+        <button id="delete-task">Delete</button>
       </div>
     `);
-        counter++;
-        $("#tick-completed").click(function () {
-            $(".each-task input[type='checkbox']:checked").each(function () {
-                $(this).siblings("p").css("color", "green");
-            })
-        })
+  });
 
-        $("#tick-not-completed").click(function () {
-            $(".each-task input[type='checkbox']:checked").each(function () {
-                $(this).siblings("p").css("color", "red");
-            })
-        })
+  // counter++;
 
-        $("#delete-task").click(function () {
-            $(".each-task input[type='checkbox']:checked").each(function () {
-                $(this).parent().remove();
-            })
-        })
+  $("#tick-completed").click(function () {
+    $(".each-task").css(function () {
+      $(".each-task").css("color", "white");
+      $(".each-task").css("background-color", "green");
     });
+  });
+
+  $("#tick-not-completed").click(function () {
+    $(".each-task").css(function () {
+      $(".each-task").css("color", "black");
+      $(".each-task").css("background-color", "red");
+    });
+  });
+
+  $("#delete-task").click(function () {
+    $(".each-task").parent().remove();
+  });
 });
+// });
